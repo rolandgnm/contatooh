6a10c0920cdc558810bc5efba4a0d330214932da
Roland Gabriel <rolandgnm@gmail.com>
2016-01-18 16:16:34 -0300

    pre first controller

--- config/express.js
+++ config/express.js
@@ -1,15 +1,18 @@
 // config/express.js
 var express = require('express');
+var home = require ( '../app/routes/home' );
 
 module.exports = function() {
   var app = express();
   
   // variável de ambiente
   app.set ( 'port' , 3000 );
+  app.set('view engine', 'ejs');
+  app.set('views', './app/views');
   
   // middleware
   app.use ( express.static( './public' ));
   
-  
+  home(app);
   return app;
 };
