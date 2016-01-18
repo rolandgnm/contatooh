cf66fe3b72ee09c3deff3ce2c14170767b7d02b2
Roland Gabriel <rolandgnm@gmail.com>
2016-01-18 16:05:33 -0300

    atualiza

--- config/express.js
+++ config/express.js
@@ -3,5 +3,13 @@ var express = require('express');
 
 module.exports = function() {
   var app = express();
+  
+  // variável de ambiente
+  app.set ( 'port' , 3000 );
+  
+  // middleware
+  app.use ( express.static( './public' ));
+  
+  
   return app;
 };
