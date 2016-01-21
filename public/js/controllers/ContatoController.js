74a1b94bbe4781195fbce7ae8b758da6498bb09c
Roland Gabriel <rolandgnm@gmail.com>
2016-01-21 17:16:33 -0300

    ngRoute funfando

---
+++ public/js/controllers/ContatoController.js
@@ -0,0 +1,7 @@
+angular.module('contatooh').controller('ContatoController', 
+    function ($scope, $routeParams) {
+        console.log($routeParams.contatoId);
+        
+    
+    
+    });
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-angular.module('contatooh', []);
+angular.module('contatooh', ['ngRoute'])
+    .config(function ($routeProvider) {
+        console.log("routing");
+        $routeProvider.when('/contatos', {
+            templateUrl: 'partials/contatos.html',
+            controller: 'ContatosController'
+
+        });
+        
+        $routeProvider.when('/contato/:contatoId', {
+            templateUrl: 'partials/contato.html',
+            controller: 'ContatoController'
+        });
+        
+        $routeProvider.otherwise({redirectTo: '/contatos'});
+    });
+    
